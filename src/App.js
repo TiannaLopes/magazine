e072615628252be2ab5c4ed472dd9850403b8077
TiannaLopes <tiannalopes@shaw.ca>
2021-06-14 12:19:23 -0400

    adding starter

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        <img src="./images/images-0.jpg" className="img-0" alt="img-0"/>
+        
+       Magazines
+      </header>
+    </div>
+  );
+}
+
+export default App;
